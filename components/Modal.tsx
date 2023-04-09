d1a53466ad96f762ffb391148f30ca41e958f5c5
Daryl Ho <darylhcw@gmail.com>
2023-04-10 05:01:07 +0800

    Add Modal Component to easily make modals!

---
+++ components/Modal.tsx
@@ -0,0 +1,82 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import styles from './Modal.module.scss'
+
+// Ensure only one modal open at a time;
+let modalIsOpen = false;
+
+interface ModalProps {
+  children: React.ReactNode;
+  closeModal?: () => void;
+}
+
+export default function Modal({children, closeModal} : ModalProps) {
+  if (modalIsOpen) return null;
+
+  useEffect(() => {
+    modalOpen();
+    return () => modalClosed();
+  }, [])
+
+  function preventBubbleUp(e : React.MouseEvent) {
+    e.stopPropagation();
+  }
+
+  return (
+    <>
+      { createPortal(
+          <div className={styles.overlay} onClick={closeModal ? closeModal : () => {}}>
+            <div className={styles.container} onClick={preventBubbleUp}>
+              {children}
+            </div>
+          </div>,
+          document.body
+      )}
+    </>
+  )
+}
+
+/**
+ * Modal Open/Close
+ * - Add overlay
+ * - Prevent body scrolling
+ * - Prevent interaction with things outside of modal.
+ *   = Extremely basic focus trap for our use case.
+ *   = We don't need to worry about accessibility for LC players.
+ */
+const FOCUSABLE = "input:not([disabled])";
+let focusRemoved : [Element, string | null][] = [] ;
+
+function modalOpen() {
+  modalIsOpen = true;
+  if (!document) return;
+
+  document.body.style.overflow = 'hidden';
+
+  const main = document.querySelector('main');
+  if (!main) return;
+
+  const focusableElements = main.querySelectorAll(FOCUSABLE);
+  focusableElements.forEach(elem => {
+    const original = elem.getAttribute("tabindex");
+    elem.setAttribute("tabindex", "-1");
+
+    focusRemoved.push([elem, original])
+  });
+}
+
+function modalClosed() {
+  modalIsOpen = false;
+  if (!document) return;
+
+  document.body.style.overflow = 'unset';
+  for (const [elem, original] of focusRemoved) {
+    if (original) {
+      elem.setAttribute("tabindex", original);
+    } else {
+      elem.removeAttribute("tabindex");
+    }
+  }
+
+  focusRemoved = [];
+}
