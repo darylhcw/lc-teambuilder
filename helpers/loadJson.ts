49fd381c10156a09898044754a0906bdd1df0743
Daryl Ho <darylhcw@gmail.com>
2024-02-18 05:43:18 +0800

    Add some extra type casting for json number data.

--- helpers/loadJson.ts
+++ helpers/loadJson.ts
@@ -10,13 +10,39 @@ export async function importIdentities() : Promise<IdentityData[]> {
   const data = await fs.readFile(idFile, "utf-8");
   const identities = JSON.parse(data);
 
-  return identities;
+  return fixIdStringCosts(identities);
 }
-
 export async function importEgos() : Promise<EgoData[]> {
   const egoFile = path.join(process.cwd(), 'public', EGO_JSON);
   const data = await fs.readFile(egoFile, "utf-8");
   const egos = JSON.parse(data);
 
+  return fixEgoStringCosts(egos);
+}
+
+
+/**
+ * Extra type casting for the numbers to be safe.
+ * The JSONs should be correct but slips have happened before.
+ */
+function fixIdStringCosts(ids: IdentityData[]) : IdentityData[] {
+  for (const id of ids) {
+    id.active.cost = Number(id.active.cost);
+    id.passive.cost = Number(id.passive.cost);
+
+    for (const skill of id.skills) {
+      skill.base = Number(skill.base);
+      skill.plus = Number(skill.plus);
+      skill.coins = Number(skill.coins);
+    }
+  }
+  return ids;
+}
+function fixEgoStringCosts(egos: EgoData[]) : EgoData[] {
+  for (const ego of egos) {
+    for (const sinCost of ego.costs) {
+      sinCost.cost = Number(sinCost.cost);
+    }
+  }
   return egos;
 }
